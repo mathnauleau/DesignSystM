1b46941ad97ea2165fc89b992731cc79397a8146
Mathieu Nauleau <123666229+mathnauleau@users.noreply.github.com>
2025-09-10 22:24:37 +0200

    Update style-dictionary.config.js
    Renamed files inside.

--- data/style-dictionary.config.js
+++ data/style-dictionary.config.js
@@ -2,21 +2,21 @@
 const StyleDictionary = require('style-dictionary');
 
 module.exports = {
-  source: ["global.json", "tokens-light.json", "tokens-dark.json"],
+  source: ["global.json", "light.json", "dark.json"],
   platforms: {
     css: {
       transformGroup: "css",
       buildPath: "build/css/",
       files: [
         {
-          destination: "variables-light.css",
+          destination: "light.css",
           format: "css/variables",
-          filter: (token) => !token.filePath.includes("tokens-dark.json")
+          filter: (token) => !token.filePath.includes("dark.json")
         },
         {
-          destination: "variables-dark.css",
+          destination: "dark.css",
           format: "css/variables",
-          filter: (token) => !token.filePath.includes("tokens-light.json")
+          filter: (token) => !token.filePath.includes("light.json")
         }
       ]
     },
@@ -25,14 +25,14 @@ module.exports = {
       buildPath: "build/js/",
       files: [
         {
-          destination: "tokens-light.js",
+          destination: "light.js",
           format: "javascript/es6",
-          filter: (token) => !token.filePath.includes("tokens-dark.json")
+          filter: (token) => !token.filePath.includes("dark.json")
         },
         {
-          destination: "tokens-dark.js",
+          destination: "dark.js",
           format: "javascript/es6",
-          filter: (token) => !token.filePath.includes("tokens-light.json")
+          filter: (token) => !token.filePath.includes("light.json")
         }
       ]
     }
